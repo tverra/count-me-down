5222625a8a8ce59dc03f477121dc0f94ae54b705
Are Johan Tverrå <are@tverra.no>
2022-07-01 16:58:05 +0200

    Update appcenter config

--- android/app/appcenter-post-clone.sh
+++ android/app/appcenter-post-clone.sh
@@ -9,7 +9,7 @@ set -e
 set -x
 
 cd ..
-git clone -b 1.22.6 https://github.com/flutter/flutter.git
+git clone -b 3.0.3 https://github.com/flutter/flutter.git
 export PATH=$(pwd)/flutter/bin:$PATH
 
 flutter doctor
